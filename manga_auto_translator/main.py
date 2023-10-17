5c90c325b1a64955011937de6762c031831dc8c6
root <root@DESKTOP-QQ6TNU8>
2023-10-16 21:50:25 -0400

    typo

--- manga_auto_translator/main.py
+++ manga_auto_translator/main.py
@@ -2,7 +2,7 @@ from argparse import ArgumentParser
 
 
 def cli():
-    argument_parser = ArgumentParser(description="Automatic translator for mangas/manwa/manhua")
+    argument_parser = ArgumentParser(description="Automatic translator for mangas/manhwa/manhua")
     argument_parser.add_argument('--input', type=str, default='./', help='Folder where the original scans are located')
     argument_parser.add_argument('--output', type=str, default='./scans-converted', help='Folder to output the converted scans')
     args = argument_parser.parse_args()
