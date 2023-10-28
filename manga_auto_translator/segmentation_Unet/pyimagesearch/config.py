5b9fdc7ff5ba1ed5041e02e9d5ac804a7cfce884
TheophileBERTELOOT <thber64@ulaval.ca>
2023-10-28 10:37:06 -0400

    add GLO-7030 segmentation model'

---
+++ manga_auto_translator/segmentation_Unet/pyimagesearch/config.py
@@ -0,0 +1,44 @@
+# import the necessary packages
+import torch
+import os
+
+# base path of the dataset
+DATASET_PATH = 'data/Manga109_released_2021_12_30/'
+# define the path to the images and masks dataset
+IMAGE_DATASET_PATH = DATASET_PATH+'images_only/'
+MASK_DATASET_PATH = os.path.join(DATASET_PATH, "Mask_only/")
+# define the test split
+TEST_SPLIT = 0.1
+# determine the device to be used for training and evaluation
+DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
+
+# determine if we will be pinning memory during data loading
+PIN_MEMORY = True if DEVICE == "cuda" else False
+
+# define the number of channels in the input, number of classes,
+# and number of levels in the U-Net model
+NUM_CHANNELS = 1
+NUM_CLASSES = 1
+NUM_LEVELS = 4
+# initialize learning rate, number of epochs to train for, and the
+# batch size
+INIT_LR = 0.001
+NUM_EPOCHS = 40
+BATCH_SIZE = 4
+# define the input image dimensions
+# INPUT_IMAGE_WIDTH = 566
+# INPUT_IMAGE_HEIGHT = 400
+#INPUT MODEL 7
+INPUT_IMAGE_WIDTH = 1654
+INPUT_IMAGE_HEIGHT = 1170
+# define threshold to filter weak predictions
+THRESHOLD = 0.3
+# define the path to the base output directory
+BASE_OUTPUT = "manga_auto_translator/segmentation_Unet/outputs"
+# define the path to the output serialized model, model training
+# plot, and testing image paths
+MODEL_PATH = os.path.join(BASE_OUTPUT, "unet_tgs_salt_model8.pth")
+PLOT_PATH = os.path.sep.join([BASE_OUTPUT, "plotUnet_loss.png"])
+PLOT_SCORE_PATH = os.path.sep.join([BASE_OUTPUT, "plotUnet_score.png"])
+TEST_PATHS = os.path.sep.join([BASE_OUTPUT, "test_paths.txt"])
+SCORE_PATH = os.path.sep.join([BASE_OUTPUT, "Unet_score.csv"])
